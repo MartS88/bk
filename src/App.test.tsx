7b0e6d7256ba2465701d3878984c0a029e88775c
Martin <papapopal522@gmail.com>
2023-10-10 16:53:38 +0300

    git-

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import Home from "./pages/home/Home";
-
+import Home from '../src/pages/home/Home';
 
 test('renders learn react link', () => {
   render(<Home />);
